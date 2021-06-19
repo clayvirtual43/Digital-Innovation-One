86c0e1af806f6ce6af27e175a539bf9088fb578f
clayvirtual43 <56259305+clayvirtual43@users.noreply.github.com>
2021-06-19 07:43:00 -0300

    Add files via upload

---
+++ Dio.Series/Dio.Series/Classes/SereiRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using Dio.Series.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dio.Series
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+        public List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualizar(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
---
+++ Dio.Series/Dio.Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dio.Series.Interfaces
+{
+    public interface IRepositorio<T>
+    {
+
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Excluir(int id);
+        void Atualizar(int id, T entidade);
+        int ProximoId();
+
+    }
+}
